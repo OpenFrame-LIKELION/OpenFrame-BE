550c7a546d8744224839e12c0c463be026ff61cb
안정후 <austinan@naver.com>
2024-11-12 22:26:51 +0900

    feat: user api

---
+++ src/main/java/com/openframe/openframe/OpenFrameApplication.java
@@ -0,0 +1,13 @@
+package com.openframe.openframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFrameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/openframe/openframe/api/user/presentation/UserController.java
@@ -0,0 +1,11 @@
+package com.openframe.openframe.api.user.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/user")
+public class UserController {
+}
