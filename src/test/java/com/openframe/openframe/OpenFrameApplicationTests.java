41bbb678c1b2a4f3e4b43b2bca5772d97fefded9
안정후 <austinan@naver.com>
2024-11-12 22:29:52 +0900

    feat: gradle settings

---
+++ src/test/java/com/openframe/openframe/OpenFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openframe.openframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFrameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
