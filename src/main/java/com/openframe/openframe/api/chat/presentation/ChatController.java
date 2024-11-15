5c0d8314b7d3286f192e8bdf89a3ddde4b6f5b59
안정후 <austinan@naver.com>
2024-11-15 20:08:04 +0900

    feat: chat controller response type

--- src/main/java/com/openframe/openframe/api/chat/presentation/ChatController.java
+++ src/main/java/com/openframe/openframe/api/chat/presentation/ChatController.java
@@ -1,8 +1,8 @@
 package com.openframe.openframe.api.chat.presentation;
 
 import com.openframe.openframe.api.chat.service.ChatService;
+import com.openframe.openframe.common.ApplicationResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +18,12 @@ public class ChatController {
     private final ChatService chatService;
 
     @GetMapping()
-    public ResponseEntity<String> getChatGptOpenApi(
+    public ApplicationResponse<String> getChatGptOpenApi(
             @RequestParam String keyword,
             @RequestParam String type
     ) throws IOException {
 
         final String chatGPTResponse = chatService.getChatGPTResponse(keyword, type);
-        return ResponseEntity.ok(chatGPTResponse);
+        return ApplicationResponse.ok(chatGPTResponse);
     }
 }
